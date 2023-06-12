25e6511e9a99ed2089b411a4ec9f28e834b26459
LiberaLongo <liberalongo@gmail.com>
2023-06-12 21:00:43 +0200

    leggibilità, non riesco a farlo con first e next... richiederebbe reimplementare le liste, devo solo capire come implementare la invisita

--- year2023/di-lena/pseudo-C-py/06-07-08-alberi/node.py
+++ year2023/di-lena/pseudo-C-py/06-07-08-alberi/node.py
@@ -5,19 +5,24 @@ class node:
 		self.info = info;
 		self.parent = None;
 		self.childrens = [];
+		self.index = 0;
 		self.tree = None; #albero a cui questo noto è associato
 
 #childerns
 	def insertChildren(self, node):
+		node.index = len(self.childrens);
 		self.childrens.append(node);
 		return node;
 
 #print
 	def print(self):
-		print(f'({self.info}', end='');
+		print(f'({self.info}, {self.index}', end='');
 		if self.childrens:
-			print(':[', end='');
+			print(', children:[ ', end='');
 			for node in self.childrens:
+				if node.index > 0:
+					print(' --> ', end='');
 				node.print();
-			print(']', end='');
+			print(' ]', end='');
 		print(')', end='');
+
